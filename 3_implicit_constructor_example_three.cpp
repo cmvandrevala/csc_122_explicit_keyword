cf186b495c978adc363d6c4fcde706bdbaba6d0e
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-10-02 11:04:47 -0500

    Improve the examples

--- 3_implicit_constructor_example_three.cpp
+++ 3_implicit_constructor_example_three.cpp
@@ -40,7 +40,14 @@ void tell_me_your_flavor(Sprinkles sprinkles)
 
 int main()
 {
-  cout << "But wait... why the heck does this work???" << endl;
+  cout << "But wait... some of these work but others don't???" << endl;
+  JellyFilling jelly_filling;
+  Doughnut doughnut;
+  SweetCoating sweet_coating;
   Sprinkles sprinkles;
-  tell_me_your_flavor(sprinkles);
+
+  // tell_me_your_flavor(jelly_filling);
+  // tell_me_your_flavor(doughnut);
+  // tell_me_your_flavor(sweet_coating);
+  // tell_me_your_flavor(sprinkles);
 }
